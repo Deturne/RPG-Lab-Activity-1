00a4a54c78ada5630257ac0dd2a56652f0576df4
Edison Li <72610128+Excailibur@users.noreply.github.com>
2025-03-10 12:03:51 -0400

    enums and character class

---
+++ Assets/Scripts/AbilityScore.cs
@@ -0,0 +1,96 @@
+﻿using System;
+public struct AbilityScore
+{
+    private int strength, dexterity, constitution, intelligence, wisdom, charisma;
+
+    public int Strength { 
+        get => strength;
+        set => strength =  Math.Clamp(value,0,10); 
+    }
+    public int Dexterity
+    {
+        get => dexterity;
+        set => dexterity = Math.Clamp(value, 0, 10);
+    }
+    public int Constitution
+    {
+        get => constitution;
+        set => constitution = Math.Clamp(value, 0, 10);
+    }
+    public int Intelligence
+    {
+        get => intelligence;
+        set => intelligence = Math.Clamp(value, 0, 10);
+    }
+    public int Wisdom
+    {
+        get => wisdom;
+        set => wisdom = Math.Clamp(value, 0, 10);
+    }
+    public int Charisma
+    {
+        get => charisma;
+        set => charisma = Math.Clamp(value, 0, 10);
+    }
+
+    public AbilityScore(int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma)
+    {
+        Strength = strength;
+        Dexterity = dexterity;
+        Constitution = constitution;
+        Intelligence = intelligence;
+        Wisdom = wisdom;
+        Charisma = charisma;
+    }
+
+    /// <summary>
+    /// Returns the modifier for the ability score
+    /// </summary>
+    /// <param name="name">Give a enum type of AbilityScoreNames </param>
+    /// <returns>A int modifer based on the current stats</returns>
+    public int getModifier(Enum_AbilityScoreNames name)
+    {
+        //Each ability has a max score of 10, and the range for the bonus is the following: 0-2 (-1), 3-4 (0), 5-6 (1), 7-8 (2), 9-10 (3).
+        int modifer = 0;
+        switch(name):
+            case Enum_AbilityScoreNames.STRENGTH:
+            modifer = Strength;
+            break;
+        case Enum_AbilityScoreNames.DEXTERITY:
+            modifer = Dexterity;
+            break;
+        case Enum_AbilityScoreNames.CONSTITUTION:
+            modifer = Constitution;
+            break;
+        case Enum_AbilityScoreNames.INTELLIGENCE:
+            modifer = Intelligence;
+            break;
+        case Enum_AbilityScoreNames.WISDOM:
+            modifer = Wisdom;
+            break;
+        case Enum_AbilityScoreNames.CHARISMA:
+            modifer = Charisma;
+            break;
+        }
+
+        if (modifer <= 2)
+        {
+            return -1;
+        }
+        else if (modifer <= 4)
+        {
+            return 0;
+        }
+        else if (modifer <= 6)
+        {
+            return 1;
+        }
+        else if (modifer <= 8)
+        {
+            return 2;
+        }
+        else
+        {
+            return 3;
+        }
+    }
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+public class Character
+{
+    #region Variables and Enums
+    [Header("Character Variables")]
+    private string _characterName;
+    private Enum_CharacterType _characterType;
+    private Enum_CharacterClass _characterClass;
+    private Enum_AbilityScore _abilityScore;
+
+    //This is h how you access enums from a class
+    //Note: The enum must be public inorder to access it from another class
+
+    //private PublicEnum.Enum1 enum1;
+    //private PublicEnum.Enum2 enum2;
+    //private PublicEnum.Enum3 enum3;
+
+    //enum1 = PublicEnum.Enum1.Value1;
+    //enum2 = PublicEnum.Enum2.Value2;
+    //enum3 = PublicEnum.Enum3.Value3;
+    #endregion
+
+    #region Properties
+    //Shorthand properties
+    public string CharacterName { get => _characterName; set => _characterName = value; }
+    public Enum_CharacterType CharacterType { get => _characterType; set => _characterType = value; }
+    public Enum_CharacterClass CharacterClass { get => _characterClass; set => _characterClass = value; }
+    public Enum_AbilityScore AbilityScore { get => _abilityScore; set => _abilityScore = value; }
+
+    //The properties above but not shorthanded
+    //public string ExpandedCharacterName
+    //{
+    //    get
+    //    {
+    //        return _characterName;
+    //    }
+    //    set
+    //    {
+    //        _characterName = value;
+    //    }
+    //}
+    #endregion
+
+    #region Constructors
+    //Using c# global variables underscore prefix convention
+    public Character(string characterName, Enum_CharacterType characterType, Enum_CharacterClass characterClass, Enum_AbilityScore abilityScore)
+    {
+        _characterName = characterName;
+        _characterType = characterType;
+        _characterClass = characterClass;
+        _abilityScore = abilityScore;
+    }
+
+    //Using this convention
+    //public Character(string characterName, Enum_CharacterType characterType, Enum_CharacterClass characterClass, Enum_AbilityScore abilityScore)
+    //{
+    //    this.characterName = characterName;
+    //    this.characterType = characterType;
+    //    this.characterClass = characterClass;
+    //    this.abilityScore = abilityScore;
+    //}
+    #endregion
+}
